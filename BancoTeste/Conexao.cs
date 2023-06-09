fb59163beecc1b4c31428dc089d28bba4872854b
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2023-06-09 12:40:53 -0400

    Comentario

---
+++ BancoTeste/Conexao.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+public static class Conexao
+{
+    static private MySqlConnection ConexaoBd;
+    public static MySqlConnection Conectar()
+    {       
+        try
+        {
+            //teste do github
+            string conexao = "server=localhost;uid=root;pwd=Triplo9!;database=BancoTeste";
+            ConexaoBd = new MySqlConnection(conexao);
+            ConexaoBd.Open();
+
+            Console.WriteLine("Conexão realizada com sucesso");
+            
+        }        
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao conectar ao banco de dados" + ex.Message);
+        }
+        return ConexaoBd;
+    }
+
+    public static void Desconectar()
+    {
+        ConexaoBd.Close();
+    }
+}
+
